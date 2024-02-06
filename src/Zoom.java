f7addb6b633c67851a3671c51198494463f72471
DragonFlyersx <mnla@itu.dk>
2024-02-06 17:41:48 +0100

    up

---
+++ src/Zoom.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Zoom {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        int zoom_amount = s.nextInt();
+        for(int i = 0; i < n;i++){
+            arr[i] = s.nextInt();
+            }
+        }
+    }
+

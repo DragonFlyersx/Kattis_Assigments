5f5bb7b3e7fc48be469c9d4e49c0211f8f9fcf15
DragonFlyersx <mnla@itu.dk>
2024-02-28 20:13:29 +0100

    Grades 25/100

--- src/Grades.java
+++ src/Grades.java
@@ -1,15 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 import edu.princeton.cs.algs4.Insertion;
 
 public class Grades {
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        int s = scanner.nextInt();
-        for(int i = 0; i < s; i++){
+    public static void main(String [] args) throws FileNotFoundException{
+        File file = new File("Data/GradeAsignment/1.in");
 
+        Scanner scanner = new Scanner(file); // use File for file
+        int s = scanner.nextInt();
+
+        // Creates the array of Students
+        Students[] AllStudents = new Students[s];
+
+
+        // adding the students to the Array
+        for(int i = 0; i < s; i++){
+            AllStudents[i] = new Students(scanner.next(), scanner.next());
         }
+
+        // Array sort with custom Comparing settings.
+
+        Arrays.sort(AllStudents, Comparator
+                .comparing((Students student) -> student.grade)
+                .thenComparing(Students -> Students.name));
+
+        //prints students
+        for(Students t : AllStudents){
+            System.out.println(t.name);
+        }
+
     }
 }
-
-
-// lav array brug . sort ? Måske dobbelt array eller arraylist?
+ class Students{
+    String name;
+    String grade;
+    public Students(String name, String grade){
+        this.name = name;
+        this.grade = grade;
+    }
+}

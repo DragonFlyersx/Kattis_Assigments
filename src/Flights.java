09733f61c3770af6f734dc89ee519b862a61ddb7
mnla <mnla@itu.dk>
2024-04-09 12:46:31 +0200

    Create Flight

---
+++ src/Flights.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Flights {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // Amount of planes coming in with a time stamp and destination seperated by a space
+        int m = scanner.nextInt(); // Amount of commands coming in
+        //create Binary Search Tree
+        for(int i = 0; i < n; i++){
+            // Take in flight split line create flight object use time as second bst
+        }
+
+    }
+}
+
+// Add BST SET time to be seconds and update the key and use value as an object.
